fd5344de0850ad0589e2b8c9feedeef9979d6bb7
CHEX <chexjs@gmail.com>
2019-12-23 03:48:34 +0200

    7

---
+++ build/precache-manifest.128d5f10da54ee0110d718479fe97349.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e94ee3e0b8ea272253942e971ff05a40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc1ae14bf64621a488d1",
+    "url": "/static/css/main.20372f4b.chunk.css"
+  },
+  {
+    "revision": "458a0a991c9f6d372e18",
+    "url": "/static/js/2.7cac5e79.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7cac5e79.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc1ae14bf64621a488d1",
+    "url": "/static/js/main.3c6572ad.chunk.js"
+  },
+  {
+    "revision": "e44a7fcd08c0c96019b3",
+    "url": "/static/js/runtime-main.29da87dd.js"
+  }
+]);
